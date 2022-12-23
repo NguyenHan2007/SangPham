5ad5c00ed4140a7b8a46af365a6519fafb53002b
Nguyen Han <nguyenthingochan20071999@gmail.com>
2022-12-24 01:16:43 +0700

    comit code

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -7,7 +7,7 @@ module.exports = {
                 let TotalPrice = 0;
                 let listproduct = await Promise.all(MyCart.idproducts.map(async idproduct => {
                     let product = await Product.findById(idproduct.idproduct).lean();
-                    TotalPrice += product.price * (1 - product.discount);
+                    TotalPrice += product.price * (1 - product.discount)*idproduct.quantity;
                     return ({...product, discountpercent: product.discount*100, quantity: idproduct.quantity});
                     
                 }));
@@ -48,20 +48,104 @@ module.exports = {
                 }
                 else
                 {
-                    const newListProduct = cart.idproducts.filter(item => item.idproduct !== idpr);
-                    var product_temp = {idproduct:idpr,quantity:quantity_temp};
-                    newListProduct.idproducts.push(product_temp);
+                    //const newListProduct = cart.idproducts.filter(item => item.idproduct !== idpr);
+                    //var product_temp = {idproduct:idpr,quantity:quantity_temp};
+                    //newListProduct.push(product_temp);
                     Product.findById(idpr)
-                        .then((pr) => {
-                            var newtotal = cart.totalprice + pr.price;
-                            Cart.findOneAndUpdate({ iduser: id }, { totalprice: newtotal, idproducts: newListProduct }, function (err, result) { })
-                        })
+                    .then((pr) => {
+                        var newtotal = cart.totalprice + pr.price;
+                        Cart.findOneAndUpdate({ iduser: id, "idproducts.idproduct":idpr }, { totalprice: newtotal, "idproducts.$.quantity": quantity_temp }, function (err, result) { })
+                    })
                 }
-                    
-                
             })
+            
             .catch((err) => console.log(err))
 },
+AddProductToCartPlus: async (req, res, id, idpr) =>//id: id của cart, vì mỗi user có 1 cart nên lấy id cart. Thêm 1 giá trị vào hàm là cmt để lưu giá trị comment
+    {
+        Cart.findOne({ iduser: id }).lean()//tìm đến giỏ hàng của người có id = id
+            .then((cart) => {
+                var check = 0;
+                var quantity_temp = 0;
+                cart.idproducts.forEach(element => {
+                    if (element.idproduct == idpr) {
+                        check += 1;
+                        quantity_temp = element.quantity +1;
+                    }
+                });
+                if(check==0)
+                {
+                    var product_temp = {idproduct:idpr,quantity:1};
+                    cart.idproducts.push(product_temp);
+                    Product.findById(idpr)//tìm đến product vừa thêm vào giỏ để lấy price(giá trị) để tính tổng tiền
+                        .then((pr) => {
+
+                            var newtotal = cart.totalprice + pr.price;
+                            Cart.findOneAndUpdate({ iduser: id }, { totalprice: newtotal, idproducts: cart.idproducts }, function (err, result) { })
+                        })
+                }
+                else
+                {
+                    //const newListProduct = cart.idproducts.filter(item => item.idproduct !== idpr);
+                    //var product_temp = {idproduct:idpr,quantity:quantity_temp};
+                    //newListProduct.push(product_temp);
+                    Product.findById(idpr)
+                    .then((pr) => {
+                        var newtotal = cart.totalprice + pr.price;
+                        Cart.findOneAndUpdate({ iduser: id, "idproducts.idproduct":idpr }, { totalprice: newtotal, "idproducts.$.quantity": quantity_temp }, function (err, result) { })
+                    })
+                }
+            req.flash('success','Product added');
+            res.redirect('back'); 
+            })
+            
+            
+            .catch((err) => console.log(err))
+},
+MinusProductToCart: async (req, res, id, idpr) =>//id: id của cart, vì mỗi user có 1 cart nên lấy id cart. Thêm 1 giá trị vào hàm là cmt để lưu giá trị comment
+    {
+        Cart.findOne({ iduser: id }).lean()//tìm đến giỏ hàng của người có id = id
+            .then((cart) => {
+                var check = 0;
+                var quantity_temp = 0;
+                cart.idproducts.forEach(element => {
+                    if (element.idproduct == idpr) {
+                        check += 1;
+                        quantity_temp = element.quantity -1;
+                    }
+                });
+                if(quantity_temp==0)
+                {
+                    Cart.findOne({ iduser: id }).lean()
+                    .then((cart) => {
+                        const newListProduct = cart.idproducts.filter(item => item.idproduct !== idpr);
+                        Product.findById(idpr)
+                            .then((pr) => {
+                                var newtotal = cart.totalprice - pr.price;
+                                Cart.findOneAndUpdate({ iduser: id }, { totalprice: newtotal, idproducts: newListProduct }, function (err, result) { })
+                            })
+                    })
+                }
+                else
+                {
+                    //const newListProduct = cart.idproducts.filter(item => item.idproduct !== idpr);
+                    //var product_temp = {idproduct:idpr,quantity:quantity_temp};
+                    //newListProduct.push(product_temp);
+                    Product.findById(idpr)
+                    .then((pr) => {
+                        var newtotal = cart.totalprice - pr.price;
+                        Cart.findOneAndUpdate({ iduser: id, "idproducts.idproduct":idpr }, { totalprice: newtotal, "idproducts.$.quantity": quantity_temp }, function (err, result) { })
+                    })
+                }
+            req.flash('success','Product added');
+            res.redirect('back'); 
+            })
+            
+            
+            .catch((err) => console.log(err))
+},
+
+
     DeleteProductFromCart: async (req, res, id, idpr) => {
         Cart.findOne({ iduser: id }).lean()
             .then((cart) => {
